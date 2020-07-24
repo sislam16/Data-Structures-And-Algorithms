9302e20009d6c71c11cf0f5e2dbba37d86e056cf
Suzette <suzetteislam@gmail.com>
2020-07-23 20:32:17 -0400

    created mergeTwoLists file

---
+++ Linked-Lists/mergeTwoLists.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    let merged = new LinkedList() 
+    let current1 = node
+    let current2 = node
+    
+    if(!current1 && !current2){
+        return false
+    }
+    while(current1 && current2){
+        if(current1 > current2){
+            let newNode = new Node()h
+        }
+    }
+    
+};
