97d05fddf5075648cf521d7bfd4b2fde0661a7d5
Suzette <suzetteislam@gmail.com>
2020-07-31 00:13:30 -0400

    changing approach to splice like directions

--- Linked-Lists/mergeTwoLists.js
+++ Linked-Lists/mergeTwoLists.js
@@ -11,20 +11,33 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    let merged = new LinkedList() 
-    let current1 = node
-    let current2 = node
+    let merge;
+    let current1 = l1
+    let current2 = l2
     
     if(!current1 && !current2){
         return false
     }
-    while(current1 && current2){
-        if(current1 > current2){
-            if(merged.head === null){
-                this[head] = new Node(current2.value)
-            }
-            
+    
+    while(current1 !== null && current2 !== null){
+        if(current1.val > current2.val){
+            merge =current1.splice(0,0)
+            current2= current2.splice(1,0,merge)
+            console.log(current2)
+            // merged.val = current2.val
+            // merged.next= current1.val
+            current1= current1.next
+            current2 = current2.next
+        } else if(current2.val > current1.val){
+             merge =current2.splice(0,0)
+            current1= current1.splice(1,0,merge)
+            console.log(current2)
+            // merged.val = current1.val
+            // merged.next =current2.val
+            current1=current1.next
+            current2 = current2.next
         }
     }
-    
+console.log(merged)  
+    return merged
 };
