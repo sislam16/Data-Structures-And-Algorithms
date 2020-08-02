6295d706e073ca743b38e8f1030d10086c0a310d
Suzette <suzetteislam@gmail.com>
2020-08-02 15:16:44 -0400

    rewrite code

--- Linked-Lists/mergeTwoLists.js
+++ Linked-Lists/mergeTwoLists.js
@@ -11,33 +11,33 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    let merge;
+    let mergedHead = null
+    let currentMerged = null
     let current1 = l1
     let current2 = l2
     
-    if(!current1 && !current2){
-        return false
-    }
+//     if(!current1 && !current2){
+//         return false
+//     }
     
     while(current1 !== null && current2 !== null){
         if(current1.val > current2.val){
-            merge =current1.splice(0,0)
-            current2= current2.splice(1,0,merge)
-            console.log(current2)
-            // merged.val = current2.val
-            // merged.next= current1.val
-            current1= current1.next
+            let newNode = new ListNode(current2.val)
+            if(!mergedHead){
+                mergedHead = newNode 
+            }
+            currentMerged = newNode            
             current2 = current2.next
-        } else if(current2.val > current1.val){
-             merge =current2.splice(0,0)
-            current1= current1.splice(1,0,merge)
-            console.log(current2)
-            // merged.val = current1.val
-            // merged.next =current2.val
+        } else{
+            let newNode = new ListNode(current1.val)
+            if(!mergedHead){
+                mergedHead = newNode
+            }
+            currentMerged.next = newNode
+            currentMerged = newNode
             current1=current1.next
-            current2 = current2.next
         }
     }
 console.log(merged)  
-    return merged
+    return mergedHead
 };
