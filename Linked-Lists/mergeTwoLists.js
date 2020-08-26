bfdc65e999004e1e8222b50649e88383e368b7ba
Suzette <suzetteislam@gmail.com>
2020-08-25 21:17:26 -0400

    merge two list update, has a bug

--- Linked-Lists/mergeTwoLists.js
+++ Linked-Lists/mergeTwoLists.js
@@ -1,43 +1,34 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
 var mergeTwoLists = function(l1, l2) {
     let mergedHead = null
     let currentMerged = null
-    let current1 = l1
-    let current2 = l2
+
+
     
-//     if(!current1 && !current2){
-//         return false
-//     }
-    
-    while(current1 !== null && current2 !== null){
-        if(current1.val > current2.val){
-            let newNode = new ListNode(current2.val)
-            if(!mergedHead){
-                mergedHead = newNode 
-            }
-            currentMerged = newNode            
-            current2 = current2.next
-        } else{
-            let newNode = new ListNode(current1.val)
+    while(l1 !== null && l2 !== null){
+        
+        if(l1.val > l2.val){
+            let newNode = new ListNode(l2.val)
             if(!mergedHead){
                 mergedHead = newNode
+                currentMerged = mergedHead
+            }
+            currentMerged.next= newNode
+            currentMerged = currentMerged.next
+            l2 = l2.next
+        } else{
+            let newNode = new ListNode(l1.val)
+            if(!mergedHead){
+                mergedHead = newNode
+                currentMerged = mergedHead
+
             }
             currentMerged.next = newNode
-            currentMerged = newNode
-            current1=current1.next
+            currentMerged = currentMerged.next
+            l1=l1.next
         }
+
     }
-console.log(merged)  
+  
+console.log(mergedHead)  
     return mergedHead
 };
