8bef23afa7ff3c6b67a0965098a12b2ec894596c
Suzette <suzetteislam@gmail.com>
2020-08-27 20:29:21 -0400

    commonChars solution

--- Hash-Tables/commonChars.js
+++ Hash-Tables/commonChars.js
@@ -1,23 +1,38 @@
-/**
- * @param {string[]} A
- * @return {string[]}
- */
 var commonChars = function(A) {
-    commonChar = []
-    currentChar; 
-    // [{'b':1, 'e':1, 'l':2, 'a':1}, 
-    //  {'l':2, 'a':1, 'b':1, 'e':1}, 
-    //  {'r':1, 'o':1,'l':2,'e':1, 'r':2}]
-    
-    for(let el of arr){
-        let elTracker = {}
-        for(i=0; i<el.length; i++){
-            if(!elTracker[el[i]]){
-                elTracker[el[i]] = 1 
+    const allWordsArr = []
+    const result = []
+
+    for(let i = 0; i < A.length; i++){
+        const currentWord = A[i]
+        const wordMap = {}
+        for(let j = 0; j < currentWord.length; j++){
+            const currChar = currentWord[j]
+            if(!wordMap[currChar]){
+                wordMap[currChar] = 1
             }else{
-                elTracker[el[i]] +=1
+               wordMap[currChar] += 1 
             }
         }
-    }
+       allWordsArr.push(wordMap) 
+    }    
     
+    const firstMap = allWordsArr[0]
+    for(let letter in firstMap){
+        let count = firstMap[letter]
+        for(i = 1; i < allWordsArr.length; i++){
+            const currMap = allWordsArr[i]
+            if(currMap[letter]){
+                if(currMap[letter] < count){
+                    count = currMap[letter]
+                }
+            }else{
+                count = 0
+            }
+        }
+        
+        for(let i = 0; i < count; i ++){
+            result.push(letter)
+        }
+    }
+    return result
 };
