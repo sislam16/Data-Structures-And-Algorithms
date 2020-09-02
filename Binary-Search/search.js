065b6a66c7e705e510c656aa8cee3939e3ab1e5b
Suzette <suzetteislam@gmail.com>
2020-09-02 00:45:53 -0400

    workingon search solution

--- Binary-Search/search.js
+++ Binary-Search/search.js
@@ -1,6 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
 var search = function(nums, target) {
     let low = 0
     let high = nums.length - 1
     let middle = Math.floor(nums.length/2)
     
+    if(target >= low && target < middle ){
+        
+        for(let i=low; i< middle; i++){
+            console.log('i',i)
+            if(target === nums[i]){
+                return nums[i]
+            }
+        }
+    }
+    
+    if(target >= middle && target < high){
+        for(let k= middle; k < high; k++){
+            console.log('k',k)
+            return nums[k]
+        }
+    }
 };
