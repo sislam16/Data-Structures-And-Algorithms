a7897880345743e52026d55509ba68dc2f4de92c
Suzette <suzetteislam@gmail.com>
2020-09-02 21:48:44 -0400

    mergeTwoList has a working solution, need to get edge cases

--- Linked-Lists/mergeTwoLists.js
+++ Linked-Lists/mergeTwoLists.js
@@ -1,11 +1,12 @@
 var mergeTwoLists = function(l1, l2) {
     let mergedHead = null
     let currentMerged = null
-
-
     
+    if(!l1 && !l2){
+        return 0
+    }
+
     while(l1 !== null && l2 !== null){
-        
         if(l1.val > l2.val){
             let newNode = new ListNode(l2.val)
             if(!mergedHead){
@@ -29,6 +30,20 @@ var mergeTwoLists = function(l1, l2) {
 
     }
   
+    while(l1 !== null){
+        let newNode = l1
+        currentMerged.next = newNode
+        currentMerged = currentMerged.next
+         l1=l1.next
+    }
+    
+      while(l2 !== null){
+        let newNode = l2
+        currentMerged.next = newNode
+        currentMerged = currentMerged.next
+         l2=l2.next
+    }
+    
 console.log(mergedHead)  
     return mergedHead
 };
