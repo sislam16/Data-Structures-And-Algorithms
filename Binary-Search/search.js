691c983c84018869f364fa9cdd3ee83f9871ba94
Suzette <suzetteislam@gmail.com>
2020-09-17 20:23:04 -0400

    working solution wo binary search problem

--- Binary-Search/search.js
+++ Binary-Search/search.js
@@ -8,27 +8,26 @@ var search = function(nums, target) {
     let high = nums.length - 1
     let middle = Math.floor(nums.length/2)
     
+    
     if(target === nums[middle]){
         return middle
     }
     
-    if(target < nums[middle] ){
-        
+    if(target < nums[middle]){   
         for(let i=low; i< middle; i++){
-            console.log('i',i)
             if(target === nums[i]){
                 return i
             }
         }
     }
     
-    if(target > middle){
-        for(let k= middle; k < high; k++){
-            console.log('k',k)
+    if(target > nums[middle]){
+        for(let k= middle; k <= high; k++){
             if(target === nums[k]){
                 return k
             }
         }
     }
-
+    
+    return -1
 };
