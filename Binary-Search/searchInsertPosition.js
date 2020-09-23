e032bf2daddeb14dc3bf025912bbfce235ca8235
Suzette <suzetteislam@gmail.com>
2020-09-22 20:32:05 -0400

    working search insert solution

--- Binary-Search/searchInsertPosition.js
+++ Binary-Search/searchInsertPosition.js
@@ -7,31 +7,41 @@ var searchInsert = function(nums, target) {
     let start = 0
     let end = nums.length -1
     let mid = Math.floor(nums.length/2)
-    let before = nums[start]
-    let after;
+   
     
     if(target === nums[mid]){
         return mid
     }
+    if(target > nums[end]){
+        return end + 1
+    }
+    
+    if(target < nums[start]){
+        return start
+    }
     
     if(target < nums[mid]){
         for(let i=0; i < mid; i++){
+            let current = nums[i];
+            let next= nums[i+1];
             if(target === nums[i]){
                 return i
             }else{
-                if(target < nums[i]){
-                    return i +1
-                }
+               if(target > current && target < next){
+                   return i+1
+               }
             }
         } 
     }
     
     if(target > nums[mid]){
-        for(let j=0; j<= end; j++){
+        for(let j=mid; j<= end; j++){
+            let curr2 = nums[j]
+            let next2 = nums[j+1]
             if(target === nums[j]){
                 return j
             } else {
-                if(target > nums[j]){
+                if(target > curr2 && target < next2){
                 return j + 1
             }
             }
