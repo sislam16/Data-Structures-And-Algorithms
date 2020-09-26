80d951ec2e3cda9076836bbbd0fd8bc37721b62a
Suzette <suzetteislam@gmail.com>
2020-09-26 10:43:43 -0400

    iterative binary search

--- Binary-Search/search.js
+++ Binary-Search/search.js
@@ -6,26 +6,15 @@
 var search = function(nums, target) {
     let low = 0
     let high = nums.length - 1
-    let middle = Math.floor(nums.length/2)
     
-    
-    if(target === nums[middle]){
-        return middle
-    }
-    
-    if(target < nums[middle]){   
-        for(let i=low; i< middle; i++){
-            if(target === nums[i]){
-                return i
-            }
-        }
-    }
-    
-    if(target > nums[middle]){
-        for(let k= middle; k <= high; k++){
-            if(target === nums[k]){
-                return k
-            }
+    while(low <= high){
+        let middle = Math.floor((low +high)/2)
+        if(target === nums[middle]){
+            return middle
+        } else if(nums[middle]> target){
+            high = middle - 1
+        } else{
+            low = middle + 1
         }
     }
     
