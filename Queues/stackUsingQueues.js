39f86728d98b08783bb7d436f63394fa872f29e2
Suzette <suzetteislam@gmail.com>
2020-10-11 18:07:27 -0400

    implemented stack using 2queues, working solution

--- Queues/stackUsingQueues.js
+++ Queues/stackUsingQueues.js
@@ -1,10 +1,9 @@
-import {Queue} from './arrayQueue'
 /**
  * Initialize your data structure here.
  */
-var MyStack = function() {
-    this.queue = new Queue()
-    this.size = 0
+var MyStack = function () {
+    this.queue1 = new Queue()
+    this.stackQueue = new Queue()
 };
 
 /**
@@ -12,45 +11,63 @@ var MyStack = function() {
  * @param {number} x
  * @return {void}
  */
-MyStack.prototype.push = function(x) {
-    this.queue.enqueue(x)// creating el at position of size num
-    this.size++ // increase stack size by1
+MyStack.prototype.push = function (x) {
+    this.queue1.enqueue(x)// creating el at position of size num
+
 };
 
 /**
  * Removes the element on top of the stack and returns that element.
  * @return {number}
  */
-MyStack.prototype.pop = function() {
-    if(this.size === 0){
+MyStack.prototype.pop = function () {
+    if (this.queue1.length === 0) {
         return null
     }
-    let poppedEl = this.queue.pop() //saving the val of last el in queue
-    this.queue[this.size-1] = null // reassigning to null
-    this.size -=1 //reducing size by 1
-    return poppedEl // return val of el removed
+    while (!this.queue1.isEmpty()) {
+        let poppedEl = this.queue1.dequeue() //saving the val of last el in queue
+        if (this.queue1.isEmpty()) {
+            this.queue1.enqueue(poppedEl)
+            break;
+        }
+        this.stackQueue.push(poppedEl)
+    }
+
+    while (!this.stackQueue.isEmpty()) {
+        this.queue1.enqueue(this.stackQueue.dequeue())
+    }
+
+    let removedEl = this.queue1.dequeue()
+    return removedEl
 };
 
 /**
  * Get the top element.
  * @return {number}
  */
-MyStack.prototype.top = function() {
-    if(this.size === 0){
-        return null
+MyStack.prototype.top = function () {
+    let el;
+    while (!this.queue1.isEmpty()) {
+        el = this.queue1.dequeue()
+        this.stackQueue.enqueue(el)
     }
-   return this.queue[this.size-1] // last el in stack but first in queue
+    while (!this.stackQueue.isEmpty()) {
+        this.queue1.enqueue(this.stackQueue.dequeue())
+    }
+
+    return el
+
 };
 
 /**
  * Returns whether the stack is empty.
  * @return {boolean}
  */
-MyStack.prototype.empty = function() {
-    return this.queue.isEmpty()
+MyStack.prototype.empty = function () {
+    return this.queue1.isEmpty()
 };
 
-/** 
+/**
  * Your MyStack object will be instantiated and called as such:
  * var obj = new MyStack()
  * obj.push(x)
