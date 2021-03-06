726ccea26d5678ec3111880c45fd680abb640317
Suzette <suzetteislam@gmail.com>
2021-03-06 16:33:06 -0500

    remove element solution

---
+++ leetcode/removeElement.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    for (let i = 0; i < nums.length; i++) {
+        while (nums[i] === val) {
+            nums.splice(i, 1)
+        }
+    }
+    return nums.length
+};
